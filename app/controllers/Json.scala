2540e7e32e594aee47db6270946156795690160c
Suhard Damien <dsuhard@gmail.com>
2015-11-23 17:38:32 +0100

    create route /hello

--- app/controllers/Json.scala
+++ app/controllers/Json.scala
@@ -1,8 +1,17 @@
 package controllers
 
-/**
- * Created by damiensuhard on 23/11/15.
- */
-class Json {
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+
+class Json extends Controller {
+
+  def hello = Action {
+    Ok(Json.obj("message" -> "hello"))
+  }
+
+  def bye = Action {
+    Ok(Json.obj("message" -> "bye"))
+  }
 
 }
